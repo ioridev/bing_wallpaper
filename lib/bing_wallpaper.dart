bca3c3499b60608310288cfa83c58b4631e3bbbc
iori <iori@iori.dev>
2022-02-04 23:46:12 +0900

    create getWallpepar

--- lib/bing_wallpaper.dart
+++ lib/bing_wallpaper.dart
@@ -1,7 +1,23 @@
 library bing_wallpaper;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class BingWallpepar {
+  Future<String> getWallpepar() async {
+    final response = await http.get(Uri.parse(
+        'https://bing.biturl.top/?resolution=3840&format=json&index=0&mkt=random'));
+    final body = utf8.decode(response.bodyBytes);
+    if (response.statusCode != 200) {
+      debugPrint('response.body$body');
+    }
+    final json = jsonDecode(body) as Map<String, dynamic>;
+    debugPrint('json$json');
+    debugPrint(json['url']);
+    final url = json['url'];
+    return url;
+  }
 }
